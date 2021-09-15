9fcd9dea15e51a6a6054ba6aa87f6c9f5541d2a0
tygrski <77369211+tygrski@users.noreply.github.com>
2021-09-15 17:54:26 -0500

    bug

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,18 @@
 // click function to select city and start search 
 addEventListener
-var cityName = document.querySelector('#textinput')
-
+function getInputValue(){
+  // Selecting the input element and get its value 
+var cityName = $('#textinput').value;
 // fetch request for weather API for selected city 
-var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tucson&appid=98c6d72f0081cffab8d92e5d78183d22&units=imperial`
+var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=98c6d72f0081cffab8d92e5d78183d22&units=imperial`
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => 
     // console.log(data));
     renderTemp(data))
+//  api 2
 
-    // display weather data
+// display weather data
 function renderTemp(weather) {
   console.log(weather)
   document.getElementById("cityName-date").textContent = `City: ${weather.name}`
@@ -23,6 +25,4 @@ function renderTemp(weather) {
 
 // append weather data to display and city to selected list
 
-// set items to local storage
-
-// get items from local storage
+// set & get items to local storage
