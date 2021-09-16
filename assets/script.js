747e583197391cb1d284a2cc6c33dbf59a3a4b4a
tygrski <77369211+tygrski@users.noreply.github.com>
2021-09-16 14:45:37 -0500

    bugs

--- assets/script.js
+++ assets/script.js
@@ -6,23 +6,29 @@ var cityName = document.getElementById('textinput').value;
 var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=98c6d72f0081cffab8d92e5d78183d22&units=imperial`
 fetch(apiUrl)
   .then(response => response.json())
-  .then(data => getCoordinates(data.coord.lat, data.coord.lon))
+  .then(data => renderTemp(data));
+  getCoordinates();
+  console.log(data);
 };
 // fetch UV index api
 function getCoordinates (lat,lon) {
   var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${ lat }&lon=${ lon }&appid=98c6d72f0081cffab8d92e5d78183d22&units=imperial`; 
   fetch(apiUrl)
   .then(response => response.json())
-  .then(data => renderTemp(data))
+  .then(data => renderUvi(data))
 };
+
 // display weather data
+function renderUvi(weather) {
+  document.getElementById("uv-index").textContent = `UV Index: ${weather.current.uvi}`
+};
 function renderTemp(weather) {
-  console.log(weather)
+  // console.log(weather)
   document.getElementById("cityName-date").textContent = `City: ${weather.name}`
   document.getElementById("temp").textContent = `Current Temperature: ${weather.main.temp} f`
   document.getElementById("wind").textContent = `Wind Speed: ${weather.wind.speed} mph`
   document.getElementById("humidity").textContent = `Humidity: ${weather.main.humidity} %`
-  document.getElementById("uv-index").textContent = `UV Index: ${weather.current.uvi}`
+ 
 };
 // append weather data to display and city to selected list
 
