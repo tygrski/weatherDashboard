6aab94a0c7dee8055f028e02e1f64076dc2106e1
tygrski <77369211+tygrski@users.noreply.github.com>
2021-09-16 13:35:01 -0500

    UV index bug

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,30 @@
 // click function to select city and start search 
-addEventListener
-function getInputValue(){
-  // Selecting the input element and get its value 
-var cityName = $('#textinput').value;
+function getInputValue() {
+// Selecting the input element and get its value 
+var cityName = document.getElementById('textinput').value;
+
 // fetch request for weather API for selected city 
 var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=98c6d72f0081cffab8d92e5d78183d22&units=imperial`
 fetch(apiUrl)
   .then(response => response.json())
-  .then(data => 
-    // console.log(data));
-    renderTemp(data))
-//  api 2
+  .then(data => {
+    console.log(data);
+    // getCoordinates(data)
+    renderTemp(data)
+    console.log(data.coord.lat)
+    console.log(data.coord.lon)
+  })
+};
+// fetch UV index api
+var lat = (data.coord.lat);
+var lon = (data.coord.lon);
+function getCoordinates () {
+  var apiUrl2 = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=98c6d72f0081cffab8d92e5d78183d22}' 
+  fetch(apiUrl2)
+  .then(response => response.json())
+  .then(data => {
+})
+};
 
 // display weather data
 function renderTemp(weather) {
@@ -21,8 +35,6 @@ function renderTemp(weather) {
   document.getElementById("humidity").textContent = `Humidity: ${weather.main.humidity} %`
   document.getElementById("uv-index").textContent = `UV Index: ${weather.current.uvi}`
 }
-
-
 // append weather data to display and city to selected list
 
 // set & get items to local storage
