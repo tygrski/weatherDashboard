a30501ac7d947388ca65b9d2e074901bdb039fee
tygrski <77369211+tygrski@users.noreply.github.com>
2021-09-16 22:40:19 -0500

    list append

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,26 @@
 // click function to select city and start search 
 function getInputValue() {
-// Selecting the input element and get its value 
-var cityName = document.getElementById('textinput').value;
-// fetch request for weather API for selected city 
-var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=98c6d72f0081cffab8d92e5d78183d22&units=imperial`
-fetch(apiUrl)
+  // Selecting the input element and get its value 
+  var cityName = document.getElementById('textinput').value;
+  // fetch request for weather API for selected city 
+  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=98c6d72f0081cffab8d92e5d78183d22&units=imperial`
+  fetch(apiUrl)
   .then(response => response.json())
   .then(data => { 
   renderTemp(data);
   getCoordinates(data.coord.lat,data.coord.lon);
   console.log(data);
-})
+  })
+  
+  
+  //create  list element and append to UL
+  var li = document.createElement("li");
+  li.innerHTML = cityName;
+  console.log(li);
+  // appendChild and add it to the list
+  var toList = document.getElementById("cityList");
+  console.log(toList);
+  toList.appendChild(li);
 };
 
 // fetch UV index api
@@ -21,31 +31,20 @@ function getCoordinates (lat,lon) {
   .then(data => renderUvi(data))
 };
 
-// display weather data
+// add UV index weather display
 function renderUvi(weather) {
-  document.getElementById("uv-index").textContent = `UV Index: ${weather.current.uvi}`
+  document.getElementById("uv-index").textContent = `UV Index: ${weather.current.uvi}`;
 };
 
+// add data to display
 function renderTemp(weather) {
-  document.getElementById("cityName-date").textContent = `City: ${weather.name}`
-  document.getElementById("temp").textContent = `Current Temperature: ${weather.main.temp} f`
-  document.getElementById("wind").textContent = `Wind Speed: ${weather.wind.speed} mph`
-  document.getElementById("humidity").textContent = `Humidity: ${weather.main.humidity} %`
- 
-};
-
-// append weather data to display and city to selected list
-document.getElementById("submitbtn").onclick = function() {
-  var text = document.getElementById("textinput").value; 
-  //create  list element
-  var li = "<li>" + text + "</li>";
-  // appendChild and add it to the list!
-  document.getElementById("citylist").appendChild(li);
-};
+  document.getElementById("cityName-date").textContent = `City: ${weather.name}`;
+  document.getElementById("temp").textContent = `Current Temperature: ${weather.main.temp} f`;
+  document.getElementById("wind").textContent = `Wind Speed: ${weather.wind.speed} mph`;
+  document.getElementById("humidity").textContent = `Humidity: ${weather.main.humidity} %`;
+ };
 
 // add 5 day forecast
 
-
 // set & get items to local storage
 
-// get from local storage
